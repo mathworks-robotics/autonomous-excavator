777d0e4838c3574d8ffe8172fe38f6e099ff619b
dryouwu <87453744+dryouwu@users.noreply.github.com>
2024-07-16 16:35:34 -0400

    Update startup.m to fix Unreal exe path issues

--- MATLABProjects/AutonomousExcavator/Scripts/helpers/startup.m
+++ MATLABProjects/AutonomousExcavator/Scripts/helpers/startup.m
@@ -1,7 +1,18 @@
 
-%% Check if the Simulation 3D Scene Configuration block points to a valid Unreal scene
+%% update all Simulation 3D Scene Configuration blocks to point to a valid Unreal scene
 
+prj = currentProject;
+unreal_path=erase(prj.RootFolder,"\MATLABProjects\AutonomousExcavator")+"\Unreal\Build\Windows\AutonomousExcavator.exe";
 load_system("ExcavatorUnrealExecutableSceneConfiguration")
+set_param("ExcavatorUnrealExecutableSceneConfiguration/Simulation 3D Scene Configuration","ProjectName",unreal_path);
+load_system("testLidarSensorPlacement")
+set_param("testLidarSensorPlacement/Excavator Scene","ProjectName",unreal_path)
+load_system("followPlannedTrajectory_MechanicsOnly")
+set_param("followPlannedTrajectory_MechanicsOnly/Excavator Scene","ProjectName",unreal_path)
+
+
+
+%% Check if the Simulation 3D Scene Configuration block points to a valid Unreal scene
 pathToUnrealProject = get_param("ExcavatorUnrealExecutableSceneConfiguration/Simulation 3D Scene Configuration", "ProjectName");
 
 if exist(pathToUnrealProject, "file") == 2
