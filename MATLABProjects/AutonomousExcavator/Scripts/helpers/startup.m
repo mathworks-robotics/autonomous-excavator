aa35f433319895b3dbea0352a5c9495a23ef4c8e
ckammer <ckammer@mathworks.com>
2024-07-23 13:53:41 +0200

    Removed unneeded files and unneeded parts of startup script

--- MATLABProjects/AutonomousExcavator/Scripts/helpers/startup.m
+++ MATLABProjects/AutonomousExcavator/Scripts/helpers/startup.m
@@ -3,23 +3,10 @@
 
 prj = currentProject;
 unreal_path=erase(prj.RootFolder,"\MATLABProjects\AutonomousExcavator")+"\Unreal\Build\Windows\AutonomousExcavator.exe";
-load_system("ExcavatorUnrealExecutableSceneConfiguration")
-set_param("ExcavatorUnrealExecutableSceneConfiguration/Simulation 3D Scene Configuration","ProjectName",unreal_path);
 load_system("testLidarSensorPlacement")
 set_param("testLidarSensorPlacement/Excavator Scene","ProjectName",unreal_path)
 load_system("followPlannedTrajectory_MechanicsOnly")
 set_param("followPlannedTrajectory_MechanicsOnly/Excavator Scene","ProjectName",unreal_path)
 
-
-
-%% Check if the Simulation 3D Scene Configuration block points to a valid Unreal scene
-pathToUnrealProject = get_param("ExcavatorUnrealExecutableSceneConfiguration/Simulation 3D Scene Configuration", "ProjectName");
-
-if exist(pathToUnrealProject, "file") == 2
-
-else 
-    open_system("ExcavatorUnrealExecutableSceneConfiguration")
-    errordlg("Could not find ""AutonomousExcavator.exe"" at the given path. Open the Simulation 3D Scene Configuration block and browse to the Unreal Executable, then save the model.")
-end
 
 clear;
